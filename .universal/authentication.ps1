994070db0e40604441e7baeb2c70c6ec43bb56e9
admin <admin>
2024-02-21 19:55:06 +0000

    Auth

--- .universal/authentication.ps1
+++ .universal/authentication.ps1
@@ -1,1 +1,1 @@
-﻿Set-PSUAuthenticationMethod -Type "OpenIDConnect" -Disabled -CallbackPath "/oidc" -ClientId "75a7c9ff-a173-49a4-9cac-54406fb70ab8" -ClientSecret "pgu8Q~WXyqTBjE7hbVAtBextRoOHnH4QZjELwaxk" -Authority "https://login.microsoftonline.com/3ee40c97-4b76-4470-a736-8481d7a2ed87"
+﻿Set-PSUAuthenticationMethod -Type "OpenIDConnect" -CallbackPath "/oidc" -ClientId "75a7c9ff-a173-49a4-9cac-54406fb70ab8" -ClientSecret "pgu8Q~WXyqTBjE7hbVAtBextRoOHnH4QZjELwaxk" -Authority "https://login.microsoftonline.com/3ee40c97-4b76-4470-a736-8481d7a2ed87"
