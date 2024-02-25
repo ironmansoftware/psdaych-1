c6b18f73c0fbbc2346f4e36e4d9a227132eabbe8
adam@ironmansoftware.onmicrosoft.com <adam@ironmansoftware.onmicrosoft.com>
2024-02-25 14:51:15 +0000

    Varaible

--- .universal/variables.ps1
+++ .universal/variables.ps1
@@ -1,2 +1,1 @@
-﻿New-PSUVariable -Name "MySecret" -Vault "AzureKeyVault" -Type "System.String" 
-New-PSUVariable -Name "TeamsWebHook" -Vault "Database" -Type "System.String"
+﻿New-PSUVariable -Name "TeamsWebHook" -Vault "Database" -Type "System.String"
