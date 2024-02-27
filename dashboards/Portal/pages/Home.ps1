2b4f6d2a47d497b9320ff84552b166a31652eba1
adam@ironmansoftware.onmicrosoft.com <adam@ironmansoftware.onmicrosoft.com>
2024-02-27 08:40:28 +0000

    Search

--- dashboards/Portal/pages/Home.ps1
+++ dashboards/Portal/pages/Home.ps1
@@ -1,20 +1,46 @@
 ﻿New-UDPage -Url "/home" -Name "Home" -Content {
-New-UDLayout -Columns 4 -Content {
-    New-UDCard -Title "Onboard New User" -Content {
-        New-UDTypography "Create a new user"
-    } -OnClick {
-        Invoke-UDRedirect "/new-user"
-    }
-    New-UDCard -Title "Request Virtual Machine" -Content {
-        New-UDTypography "Request a new virtual machine"
-    } -OnClick {
-        Invoke-UDRedirect "/request-virtual-machine"
-    }
-    Protect-UDSection -Role "Administrator" -Content {
-        New-UDCard -Title "View Cost Analysis" -Content {
-            New-UDTypography "View the latest cost analysis report."
+$Services = @(
+    @{
+        Title   = "Onboard New User"
+        Content = New-UDCard -Title "Onboard New User" -Content {
+            New-UDTypography "Create a new user"
         } -OnClick {
-            Invoke-UDRedirect "/cost-analysis"
+            Invoke-UDRedirect "/new-user"
+        }
+    }
+    @{
+        Title   = "Request Virtual Machine"
+        Content = New-UDCard -Title "Request Virtual Machine" -Content {
+            New-UDTypography "Request a new virtual machine"
+        } -OnClick {
+            Invoke-UDRedirect "/request-virtual-machine"
+        }
+    }
+    @{
+        Title   = "View Cost Analysis"
+        Content = Protect-UDSection -Role "Administrator" -Content {
+            New-UDCard -Title "View Cost Analysis" -Content {
+                New-UDTypography "View the latest cost analysis report."
+            } -OnClick {
+                Invoke-UDRedirect "/cost-analysis"
+            }
+        }
+    }
+)
+
+New-UDStack -FullWidth -Direction column -Children {
+    New-UDTextbox -Label Search -OnEnter {
+        $Page:Search = $EventData
+        Sync-UDElement -Id 'results'
+    } -FullWidth
+
+    New-UDDivider
+
+    New-UDDynamic -Id 'results' -Content {
+        New-UDLayout -Columns 4 -Content {
+            $Services | Where-Object { $Page:Search -eq $null -or $_.Title.Contains($Page:Search) } | ForEach-Object {
+                $_.Content
+            }
         }
     }
 }
